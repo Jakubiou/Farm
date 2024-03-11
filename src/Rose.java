1cb5f894141d7c507b605c3874a390145e287206
sramek4 <sramekl4@spsejecna.cz>
2024-03-11 11:02:19 +0100

    added new class Rose

--- src/Rose.java
+++ src/Rose.java
@@ -2,4 +2,5 @@ public class Rose extends Flower{
     public Rose(String name, double price, double neededArea, double chanceOfGrowth) {
         super(name, price, neededArea, chanceOfGrowth);
     }
+
 }
