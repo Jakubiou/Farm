c315f4a747e58c1121b156c3c9c90682ec75a74d
sramek4 <sramekl4@spsejecna.cz>
2024-03-11 11:02:05 +0100

    added new class Cactus

--- src/Cactus.java
+++ src/Cactus.java
@@ -1,5 +1,6 @@
 public class Cactus extends Flower{
     public Cactus(String name, double price, double neededArea, double chanceOfGrowth) {
         super(name, price, neededArea, chanceOfGrowth);
+
     }
 }
